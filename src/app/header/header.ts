10e362ef78d7677ef27f003946a1780d23fdc470
WorldChris380 <christian.boehme.private@gmail.com>
2025-11-02 18:03:00 +0100

    basic burger menu implemented

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrl: './header.scss',
 })
 export class Header {
+  @ViewChild('burgerMenuOpenIcon', { static: true }) burgerMenuOpenIcon!: ElementRef<HTMLButtonElement>;
+  @ViewChild('burgerMenuCloseIcon', { static: true }) burgerMenuCloseIcon!: ElementRef<HTMLButtonElement>;
+  @ViewChild('burgerMenuContent', { static: true }) burgerMenuContent!: ElementRef<HTMLDivElement>;
 
+  toggleBurgerMenu() {
+    if (window.onchange || this.burgerMenuOpenIcon.nativeElement.style.display == 'none') {
+      this.closeBurgerMenu();
+    }
+    else {
+      this.openBurgerMenu();
+    }
+  }
+
+  openBurgerMenu() {
+    this.burgerMenuOpenIcon.nativeElement.style.display = 'none';
+    this.burgerMenuCloseIcon.nativeElement.style.display = 'flex';
+    this.burgerMenuContent.nativeElement.style.display = 'flex';
+  }
+
+  closeBurgerMenu() {
+    this.burgerMenuCloseIcon.nativeElement.style.display = 'none';
+    this.burgerMenuOpenIcon.nativeElement.style.display = 'flex';
+    this.burgerMenuContent.nativeElement.style.display = 'none';
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    const width = (event.target as Window).innerWidth;
+    this.closeBurgerMenu()
+  }
 }
