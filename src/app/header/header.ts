af4f91cba27474f8f6b61b61fe9d930a0a3b2fed
WorldChris380 <christian.boehme.private@gmail.com>
2025-11-03 06:36:32 +0100

    finetuning darkmode

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ElementRef, HostListener } from '@angular/core';
+import { Component, ViewChild, ElementRef, HostListener, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   standalone: true,
@@ -15,16 +15,33 @@ export class Header {
   @ViewChild('lightIconBurger', { static: true }) lightIconBurger!: ElementRef<HTMLSpanElement>;
 
   ngOnInit() {
+    // Set theme to local storage value
     if (typeof window !== 'undefined' && window.localStorage) {
       const theme = localStorage.getItem('data-theme');
       if (theme === 'dark') {
-        this.moonIcon.nativeElement.style.display = 'none';
-        this.lightIcon.nativeElement.style.display = 'flex';
+        this.turnOnLightMode();
         document.body.classList.add('dark-mode');
       } else {
-        this.moonIcon.nativeElement.style.display = 'flex';
-        this.lightIcon.nativeElement.style.display = 'none';
+        this.turnOnDarkMode();
         document.body.classList.remove('dark-mode');
+      }
+    }
+  }
+
+  toggleDropdown(menu: string) {
+    const dropdown = document.getElementById(`dropdown-${menu}`);
+    if (dropdown) {
+      // Überprüfe, ob das Dropdown bereits geöffnet ist
+      const isOpen = dropdown.style.maxHeight && dropdown.style.maxHeight !== '0px';
+
+      if (isOpen) {
+        // Dropdown schließen
+        dropdown.style.maxHeight = '0';
+        dropdown.style.opacity = '0';
+      } else {
+        // Dropdown öffnen
+        dropdown.style.maxHeight = '500px';
+        dropdown.style.opacity = '1';
       }
     }
   }
@@ -59,19 +76,27 @@ export class Header {
 
   toggleDarkMode() {
     if (this.moonIcon.nativeElement.style.display !== 'none') {
-      this.moonIcon.nativeElement.style.display = 'none';
-      this.lightIcon.nativeElement.style.display = 'flex';
-      this.moonIconBurger.nativeElement.style.display = 'none';
-      this.lightIconBurger.nativeElement.style.display = 'flex';
+      this.turnOnLightMode();
       document.body.classList.add('dark-mode');
       localStorage.setItem("data-theme", "dark");
     } else {
-      this.moonIcon.nativeElement.style.display = 'flex';
-      this.lightIcon.nativeElement.style.display = 'none';
-      this.moonIconBurger.nativeElement.style.display = 'flex';
-      this.lightIconBurger.nativeElement.style.display = 'none';
+      this.turnOnDarkMode();
       document.body.classList.remove('dark-mode');
       localStorage.setItem("data-theme", "light");
     }
+  }
+
+  turnOnLightMode() {
+    this.moonIcon.nativeElement.style.display = 'none';
+    this.lightIcon.nativeElement.style.display = 'flex';
+    this.moonIconBurger.nativeElement.style.display = 'none';
+    this.lightIconBurger.nativeElement.style.display = 'flex';
+  }
+
+  turnOnDarkMode() {
+    this.moonIcon.nativeElement.style.display = 'flex';
+    this.lightIcon.nativeElement.style.display = 'none';
+    this.moonIconBurger.nativeElement.style.display = 'flex';
+    this.lightIconBurger.nativeElement.style.display = 'none';
   }
 }
